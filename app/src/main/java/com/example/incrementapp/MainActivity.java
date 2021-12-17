57de8e92cfbad47bb70eea9951d1bc203ad5b785
HumailShahzad <humailshahzad1@gmail.com>
2021-12-17 12:26:43 -0800

    App bar added

--- app/src/main/java/com/example/incrementapp/MainActivity.java
+++ app/src/main/java/com/example/incrementapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.incrementapp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,9 +16,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
-
+import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar myToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         Button b1=(Button) findViewById(R.id.button);
         EditText t1=findViewById(R.id.textView);
         TextView t2=(TextView)findViewById(R.id.textView2);
+        myToolbar = (Toolbar) findViewById(R.id.toolbar3);
+        setSupportActionBar(myToolbar);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,5 +51,23 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.google.com/"));
+                startActivity(intent);
+            case R.id.action_tasks:
+                Intent intent1 = new Intent(Intent.ACTION_VIEW,Uri.parse("https://github.com/HumailShahzad/Assignment3.git"));
+                startActivity(intent1);
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
